aa6d1051963542c4d52b9ef48e2630176b5c5f92
tonythomascn <tonythomascn@gmail.com>
2017-03-23 14:14:21 -0400

    fix:usr: reformat output, add run script:

--- gpu_bench/gpu_SSSP/main.cpp
+++ gpu_bench/gpu_SSSP/main.cpp
@@ -77,8 +77,8 @@ void output(vector<uint32_t> & vproplist)
 //==============================================================//
 int main(int argc, char * argv[])
 {
-    graphBIG::print();
-    cout<<"Benchmark: GPU SSSP\n";
+//    graphBIG::print();
+//    cout<<"Benchmark: GPU SSSP\n";
 
     argument_parser arg;
     gBenchPerf_event perf;
@@ -96,7 +96,7 @@ int main(int argc, char * argv[])
 
     double t1, t2;
 
-    cout<<"loading data... \n";
+//    cout<<"loading data... \n";
 
     t1 = timer::get_usec();
     size_t vertex_num, edge_num;
@@ -120,10 +120,11 @@ int main(int argc, char * argv[])
 #endif    
     t2 = timer::get_usec();
 
-    cout<<"== "<<vertex_num<<" vertices  "<<edge_num<<" edges\n";
+//    cout<<"== "<<vertex_num<<" vertices  "<<edge_num<<" edges\n";
     
 #ifndef ENABLE_VERIFY
-    cout<<"== time: "<<t2-t1<<" sec\n";
+    cout << t2 - t1 << ",";
+//    cout<<"== time: "<<t2-t1<<" sec\n";
 #else
     (void)t1;
     (void)t2;
@@ -147,10 +148,11 @@ int main(int argc, char * argv[])
     t2 = timer::get_usec();
     
 
-    cout<<"\nGPU SSSP finish: \n";
-    cout<<"== "<<vertex_num<<" vertices  "<<edge_num<<" edges\n";
+//    cout<<"\nGPU SSSP finish: \n";
+//    cout<<"== "<<vertex_num<<" vertices  "<<edge_num<<" edges\n";
 #ifndef ENABLE_VERIFY
-    cout<<"== time: "<<t2-t1<<" sec\n";
+    cout << t2 - t1 << "\n";
+//    cout<<"== time: "<<t2-t1<<" sec\n";
 #endif
 #ifdef EXTERNAL_CSR
     string refile = path + "/result.array";
@@ -162,7 +164,7 @@ int main(int argc, char * argv[])
     output(vproplist);
 #endif
 
-    cout<<"==================================================================\n";
+//    cout<<"==================================================================\n";
     return 0;
 }  // end main
 
--- gpu_bench/gpu_SSSP/sssp_data_thread_centric.cu
+++ gpu_bench/gpu_SSSP/sssp_data_thread_centric.cu
@@ -256,11 +256,11 @@ void cuda_SSSP(uint64_t * vertexlist,
     cudaEventSynchronize(stop_event);
     cudaEventElapsedTime(&d2h_copy_time, start_event, stop_event);
 
-    printf("== iteration #: %d\n", curr);
+//    printf("== iteration #: %d\n", curr);
 #ifndef ENABLE_VERIFY
-    printf("== host->device copy time: %f ms\n", h2d_copy_time);
-    printf("== device->host copy time: %f ms\n", d2h_copy_time);
-    printf("== kernel time: %f ms\n", kernel_time);
+//    printf("== host->device copy time: %f ms\n", h2d_copy_time);
+//    printf("== device->host copy time: %f ms\n", d2h_copy_time);
+//    printf("== kernel time: %f ms\n", kernel_time);
 #endif
     cudaEventDestroy(start_event);
     cudaEventDestroy(stop_event);
