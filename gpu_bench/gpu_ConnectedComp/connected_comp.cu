99071404427a6906695077a90912d536b7454a3a
Tony Liu <tonythomascn@gmail.com>
2017-03-28 16:31:12 -0400

    fix:usr: update output format

--- gpu_bench/gpu_ConnectedComp/connected_comp.cu
+++ gpu_bench/gpu_ConnectedComp/connected_comp.cu
@@ -228,6 +228,9 @@ void cuda_connected_comp(uint64_t * vertexlist,
     cudaEventSynchronize(stop_event);
     cudaEventElapsedTime(&d2h_copy_time, start_event, stop_event);
 #ifndef ENABLE_VERIFY
+printf("%f,", h2d_copy_time / 1000);
+printf("%f,", d2h_copy_time / 1000);
+printf("%f,", kernel_time / 1000);
 //    printf("== host->device copy time: %f ms\n", h2d_copy_time);
 //    printf("== device->host copy time: %f ms\n", d2h_copy_time);
 //    printf("== kernel time: %f ms\n", kernel_time);
