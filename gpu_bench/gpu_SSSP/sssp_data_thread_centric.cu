2f11933926e8c1b49b9bb317e43ea8e5f29127cf
Tony Liu <tonythomascn@gmail.com>
2017-03-28 16:39:59 -0400

    fix:usr: update output

--- gpu_bench/gpu_SSSP/sssp_data_thread_centric.cu
+++ gpu_bench/gpu_SSSP/sssp_data_thread_centric.cu
@@ -258,6 +258,9 @@ void cuda_SSSP(uint64_t * vertexlist,
 
 //    printf("== iteration #: %d\n", curr);
 #ifndef ENABLE_VERIFY
+printf("%f,", h2d_copy_time / 1000);
+printf("%f,", d2h_copy_time / 1000);
+printf("%f,", kernel_time / 1000);
 //    printf("== host->device copy time: %f ms\n", h2d_copy_time);
 //    printf("== device->host copy time: %f ms\n", d2h_copy_time);
 //    printf("== kernel time: %f ms\n", kernel_time);
