6aa746077c49a0a957d4d10f2cad96356d490f0f
tonythomascn <tonythomascn@users.noreply.github.com>
2017-03-28 14:53:25 -0400

    fix:usr: hard code weak scaling from 1 to 1024

--- benchmark/bench_triangleCount/tc.cpp
+++ benchmark/bench_triangleCount/tc.cpp
@@ -332,7 +332,7 @@ int main(int argc, char * argv[])
 
     size_t threadnum;
     arg.get_value("threadnum",threadnum);
-    cout << threadnum << ",";
+//    cout << threadnum << ",";
     arg.get_value("maxiter",maxiter);
 #ifdef SIM
     arg.get_value("beginiter",beginiter);
@@ -360,11 +360,20 @@ int main(int argc, char * argv[])
     uint64_t vertex_num = graph.num_vertices();
     uint64_t edge_num = graph.num_edges();
     t2 = timer::get_usec();
-//    cout<<"== "<<vertex_num<<" vertices  "<<edge_num<<" edges\n";
+//    cout<<"== "<<vertex_num<<" vertices  "<<edge_num<<" edges\n";      
+    double loading_time = t2 - t1;
 #ifndef ENABLE_VERIFY
-    cout << t2 - t1 << ",";
+//    cout << t2 - t1 << ",";
 //    cout<<"== time: "<<t2-t1<<" sec\n";
 #endif
+    //Scale the thread num from 1 to 2^10
+    for (unsigned j = 0; j < 11; j++)
+    {
+        //reset elapse_time for each thread num config
+        elapse_time = 0;
+        threadnum = (int) pow(2, j);
+        cout << threadnum << ",";
+        
 
 //    cout<<"\npreparing neighbor sets..."<<endl;
     vector<unsigned> workset;
@@ -400,9 +409,13 @@ int main(int argc, char * argv[])
         elapse_time += t2 - t1;
         if ((i+1)<run_num) reset_graph(graph);
     }
+        cout << loading_time << "," << elapse_time/run_num << endl;
+        //after each run, reset graph
+        reset_graph(graph);
+    }
 //    cout<<"== total triangle count: "<<tcount<<endl;
 #ifndef ENABLE_VERIFY
-    cout << elapse_time/run_num<<"\n";
+//    cout << elapse_time/run_num<<"\n";
 //    cout<<"== time: "<<elapse_time/run_num<<" sec\n";
 //    if (threadnum == 1)
 //        perf.print();
