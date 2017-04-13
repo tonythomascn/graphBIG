5bea653419d5d2b128acc2433d6c5cb5c0439ba3
tonythomascn <tonythomascn@users.noreply.github.com>
2017-04-13 12:22:30 -0400

    fix:usr: add frame api

--- benchmark/bench_shortestPath/sssp.cpp
+++ benchmark/bench_shortestPath/sssp.cpp
@@ -22,6 +22,7 @@
 #endif
 
 #define MY_INFINITY 0xfff0
+#include "ittnotify.h"
 
 using namespace std;
 size_t beginiter = 0;
@@ -315,8 +316,13 @@ int main(int argc, char * argv[])
     if (run_num==0) run_num = 1;
     double elapse_time = 0;
     
+    __itt_domain* pD = __itt_domain_create( "bfs" );
+ 
+    pD->flags = 1; /* enable domain */
+    
     for (unsigned i=0;i<run_num;i++)
     {
+        __itt_frame_begin_v3(pD, NULL);
         t1 = timer::get_usec();
 
         if (threadnum==1)
@@ -325,6 +331,8 @@ int main(int argc, char * argv[])
             parallel_sssp(graph, root, threadnum, perf_multi, i);
         
         t2 = timer::get_usec();
+        __itt_frame_end_v3(pD, NULL);
+        
         elapse_time += t2-t1;
         if ((i+1)<run_num) reset_graph(graph);
     }
