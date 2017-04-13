9148fed5122fd7493d1f1f0b8beb3f332b2b9d0e
tonythomascn <tonythomascn@users.noreply.github.com>
2017-04-13 11:53:20 -0400

    fix:usr: add frame api

--- benchmark/bench_degreeCentr/dc.cpp
+++ benchmark/bench_degreeCentr/dc.cpp
@@ -15,6 +15,8 @@
 #include "HMC.h"
 #endif
 
+#include "ittnotify.h"
+
 using namespace std;
 size_t beginiter = 0;
 size_t enditer = 0;
@@ -221,8 +223,14 @@ int main(int argc, char * argv[])
     if (run_num==0) run_num = 1;
     double elapse_time = 0;
     
+    __itt_domain* pD = __itt_domain_create( "bfs" );
+ 
+    pD->flags = 1; /* enable domain */
+
+    
     for (unsigned i=0;i<run_num;i++)
     {
+        __itt_frame_begin_v3(pD, NULL);
         // Degree Centrality
         t1 = timer::get_usec();
         
@@ -232,6 +240,8 @@ int main(int argc, char * argv[])
             parallel_dc(graph, threadnum, perf_multi, i);
 
         t2 = timer::get_usec();
+        __itt_frame_end_v3(pD, NULL);
+        
         elapse_time += t2-t1;
         if ((i+1)<run_num) reset_graph(graph);
     }
